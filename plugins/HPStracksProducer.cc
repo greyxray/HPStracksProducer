743eeb6a45b889c6fe3761db3947b8d56e75ba04
Olena Hlushchenko <greyxray@gmail.com>
2017-08-03 17:41:27 +0200

    Remove duplicated inclusions

--- plugins/HPStracksProducer.cc
+++ plugins/HPStracksProducer.cc
@@ -34,11 +34,7 @@
 
 
 ////
-#include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/one/EDAnalyzer.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/EDGetToken.h"
 
 // #include "CondFormats/JetMETObjects/interface/JetCorrectionUncertainty.h"
